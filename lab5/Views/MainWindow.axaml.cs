eadf965c7b6ce84196db6d4b96085152d0272a34
AE152 <ggoollll35@gmail.com>
2022-03-17 22:26:54 +0700

    Добавьте файлы проекта.

---
+++ lab5/Views/MainWindow.axaml.cs
@@ -0,0 +1,87 @@
+using Avalonia.Controls;
+using Avalonia;
+using System;
+using System.IO;
+using Avalonia.Markup.Xaml;
+using lab5.ViewModels;
+using Avalonia.Interactivity;
+
+namespace lab5.Views
+{
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.FindControl<Button>("OpenFileNow").Click += async delegate
+            {
+                var taskPath = new OpenFileDialog()
+                {
+                    Title = "Open File",
+                    Filters = null
+                }.ShowAsync((Window)this.VisualRoot);
+
+                string[]? path = await taskPath;
+
+                var context = this.DataContext as MainWindowViewModel;
+                context.Text = string.Join(@"\", path);
+
+                using (StreamReader sr = File.OpenText(context.Text))
+                {
+                    string s = "";
+                    string temp;
+                    bool flag = true;
+                    while (flag)
+                    {
+                        temp = sr.ReadLine();
+                        if (temp != null)
+                        {
+                            s += temp;
+                            temp = " ";
+                        }
+                        else flag = false;
+                    }
+                    string p = s;
+                    context.Text = string.Join(@" ", s);
+                }
+            };
+
+            this.FindControl<Button>("SaveFileNow").Click += async delegate
+            {
+                var taskGetPath = new OpenFileDialog()
+                {
+                    Title = "Save File",
+                    Filters = null
+                }.ShowAsync((Window)this.VisualRoot);
+
+                string[]? pathToFile = await taskGetPath;
+                var contex = this.DataContext as MainWindowViewModel;
+                if (pathToFile != null)
+                {
+                    using (StreamWriter writer = new StreamWriter(string.Join(@"\", pathToFile), false))
+                    {
+                        await writer.WriteLineAsync(contex.RegularResult);
+                    }
+
+                }
+            };
+        }
+
+        private void MyClickEventHandler(object sender, RoutedEventArgs e)
+        {
+            var v = new RegexWindow();
+            v.OkNotify += delegate (string str)
+            {
+                var contex = this.DataContext as MainWindowViewModel;
+                contex.Pattern = str;
+                contex.RegularResult = contex.FindRegular();
+
+            };
+            v.Show((Window)this.VisualRoot);
+        }
+    }
+}
---
+++ lab5/Views/RegexWindow.axaml.cs
@@ -0,0 +1,40 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using ReactiveUI;
+using System;
+
+
+namespace lab5.Views
+{
+    public partial class RegexWindow : Window
+    {
+        public delegate void OkHandler(string message);
+        public event OkHandler? OkNotify;
+        public RegexWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            this.FindControl<Button>("OkeyNow").Click += async delegate
+            {
+                OkNotify(this.Find<TextBox>("textRegVr").Text);
+                Close();
+
+            };
+            this.FindControl<Button>("CancelNow").Click += async delegate
+            {
+                Close();
+
+
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+    }
+}
