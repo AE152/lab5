e6394b67d11592f95099b2009902aac3f6fe566e
AE152 <ggoollll35@gmail.com>
2022-03-24 22:26:20 +0700

    исправил фылет при вводе regex без текста

--- lab5/Models/RegexCl.cs
+++ lab5/Models/RegexCl.cs
@@ -14,11 +14,15 @@ namespace RegexCl.Models
         public static string? FindRegexInText(string text, string pattern)
         {
             string result = "";
-            Regex r = new Regex(pattern);
-            MatchCollection m = r.Matches(text);
-            foreach (Match x in m)
+            if (pattern == null) { return null; }
+            else
             {
-                result += (x.Value + "\n");
+                Regex r = new Regex(pattern);
+                MatchCollection m = r.Matches(text);
+                foreach (Match x in m)
+                {
+                    result += (x.Value + "\n");
+                }
             }
             return result;
         }
